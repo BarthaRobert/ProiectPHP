93573de82553d01300063d29feb9e9bc6a246eb1
Bartha Robert <33524939+BarthaRobert@users.noreply.github.com>
2019-02-02 15:32:42 +0200

    Delete dbconnect.php

--- BloodBankPHPLAB/classes/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "dbconfig.php";
-    $con = mysqli_connect(host,name,pass,dbname);
-    if(!$con)
-    {
-        header('Location: http://localhost/BloodBankPHPLAB//error.php');
-    }
-?>
