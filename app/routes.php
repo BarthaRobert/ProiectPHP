05590a4e608c24ecca35f3f0b45f6ea20f896b62
Bartha Robert <33524939+BarthaRobert@users.noreply.github.com>
2019-02-03 22:50:06 +0200

    Delete routes.php

--- app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$routes = [
-    '/pages/about-us' => ['controller' => 'PageController', 'action' => 'aboutUsAction'],
-    '/user/{id}' => ['controller' => 'UserController', 'action' => 'showAction'],
-    '/user/edit/{id}' => ['controller' => 'UserController', 'action' => 'showAction', 'guard' => 'Authenticated'],
-    '/pages/login'=>['controller'=>'LoginController','action'=>'loginAction'],
-    '/pages/register'=>['controller'=>'RegisterController','action'=>'registerAction']
-];
