70dbd520ad18965d6567a2863ba9c8957b0019d8
Bartha Robert <33524939+BarthaRobert@users.noreply.github.com>
2019-02-03 16:26:16 +0200

    PHP-Lab without framework

---
+++ BloodBankPHPLAB/login_error.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    include "components/header.php";
+?>
+<div class="cover">
+</div>
+<div class="myrow1">
+    <div class="container">
+        <h1 class="heading-reg">Autentificare esuata</h1>
+        <h3 class="heading-reg">A aparut o eroare in momentul autentificarii..</h3>
+        <button class="banner-try">Incearca din nou !</button>
+    </div>
+</div>
+<br><br>
+<?php 
+    include "components/footer.php";
+?>
