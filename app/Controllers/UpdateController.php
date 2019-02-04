14c2adc5fa767a83b21324c46d23b69ef5456c07
Bartha Robert <33524939+BarthaRobert@users.noreply.github.com>
2019-02-04 18:53:49 +0200

    add.html+edit.html / UpdateController functions implement

--- app/Controllers/UpdateController.php
+++ app/Controllers/UpdateController.php
@@ -11,7 +11,13 @@ use App\Models\Update;
  */
 class UpdateController extends Controller
 {
-    
+
+    private  $userId;
+
+    private $updates;
+
+    private $editUpdateId;
+
     private $updateRepository;
 
     public function __construct(){
@@ -25,7 +31,7 @@ class UpdateController extends Controller
         return $this->view("updates/updates.html",['updates' => $updates]);
     }
 
-    public function showUpdatesAction() {   
+    public function showUpdatesAction() {
         $errors =[];
 
             if(isset($_POST['login-submit'])) {
@@ -35,7 +41,7 @@ class UpdateController extends Controller
                 $errors[] = "An e-mail must be introduced!";
                 $validLogin =false;
             }
-                   
+
             if(!isset($_POST['password']) || $_POST['password'] == ''){
                 $errors[] = "A password must be introduced!";
                 $validLogin =false;
@@ -55,16 +61,89 @@ class UpdateController extends Controller
         return $this->view("login/login.html", ['errors'=>$errors]);
     }
 
-    
+    public function doUpdate() {
+        $this->updateRepository->update($this->params[0]);
+        header('Location: /');
+    }
+
+    private function createPage(){
+        return $this->view("updates/add.html");
+    }
+
+    public function editPage(){
+        $editUpdateId= $this->params[0];
+        if($editUpdateId)
+            $update= $this->updateRepository->getUpdateById($editUpdateId);
+        return $this->view("updates/edit.html",['update' => $update]);
+    }
+
+    private function createUpdate(){
+        $errros =[];
+
+        if(isset($_POST['save-submit'])) {
+            $validAdd =true;
+
+            if(!isset($_POST['applicationName']) || $_POST['applicationName'] == ''){
+                $errors[] = "An application name must be introduced!";
+                $validAdd =false;
+            }
+
+            if(!isset($_POST['every']) || $_POST['every']=='' ){
+                $errors[] = "An every must be introduced!";
+                $validAdd =false;
+            }
+
+            if(!isset($_POST['lastUpdate']) || $_POST['lastUpdate'] == ''){
+                $errors[] = "A last update must be introduced!";
+                $validAdd =false;
+            }
+
+            if($validAdd){
+                $update = new Update(null, $_POST['applicationName'],$_POST['applicationName'], $_POST['lastUpdate'],0, $userId);
+                $updates[] = $this->doCreate($update);
+                $this->updatesPage();
+                return $this->view("updates/add.html", ['errors'=>['Edit successfully!'],'updates' => $updates]);
+            }
+
+        }
+        return $this->view("login/register.html", ['errors'=>['Edit failed!']]);
+    }
+
+    private function editUpdate(){
+        $errros =[];
+
+        if(isset($_POST['save-submit'])) {
+            $validAdd =true;
+
+            if(!isset($_POST['applicationName']) || $_POST['applicationName'] == ''){
+                $errors[] = "An application name must be introduced!";
+                $validAdd =false;
+            }
+
+            if(!isset($_POST['every']) || $_POST['every']=='' ){
+                $errors[] = "An every must be introduced!";
+                $validAdd =false;
+            }
+
+            if(!isset($_POST['lastUpdate']) || $_POST['lastUpdate'] == ''){
+                $errors[] = "A last update must be introduced!";
+                $validAdd =false;
+            }
+
+            if($validAdd){
+                $update = new Update(null, $_POST['applicationName'],$_POST['applicationName'], $_POST['lastUpdate'],0, $userId);
+                $updates[] = $this->doEdit($update);
+                return $this->view("updates/add.html", ['errors'=>['Save successfully!'],'updates' => $updates]);
+            }
+
+        }
+        return $this->view("login/register.html", ['errors'=>$errors]);
+    }
     public function logoutAction(){
         session_unset();
         header('Location: /');
     }
 
-    private function doUpdate($update) {
-        $this->updateRepository->update($update);
-    }
- 
     private function doCreate($update) {
         $this->updateRepository->create($update);
     }
@@ -72,7 +151,7 @@ class UpdateController extends Controller
     private function doEdit($update) {
         $this->updateRepository->edit($update);
     }
-     
+
     private function doDeleted($update){
         $this->updateRepository->delete($update);
     }
--- app/routes.php
+++ app/routes.php
@@ -2,13 +2,17 @@
 $routes = [
     '/page/about-us' => ['controller' => 'PageController', 'action' => 'aboutUsAction'],
     '/user/{id}' => ['controller' => 'UserController', 'action' => 'showAction'],
-    '/user/edit/{id}' => ['controller' => 'UserController', 'action' => 'showAction', 'guard' => 'Authenticated'],
+    '/user/edit' => ['controller' => 'UserController', 'action' => 'showAction', 'guard' => 'Authenticated'],
     '/login' => ['controller' => 'LoginController', 'action' => 'loginPage'],
     '/login/auth' => ['controller' => 'LoginController', 'action' => 'loginAction'],
     '/logout' => ['controller' => 'LoginController', 'action' => 'logoutAction'],
     '/register' => ['controller' => 'LoginController', 'action' => 'registerPage'],
     '/register/reg' => ['controller' => 'LoginController', 'action' => 'registerAction'],
     '/updates' => ['controller' => 'UpdateController', 'action' => 'updatesPage'],
-    '/updates/add'=>['controller' => 'UpdateController' , 'action' => 'showUpdatesAction'],
+    '/updates/{id}/update' => ['controller' => 'UpdateController', 'action' => 'doUpdate','guard' => 'Authenticated'],
+    '/updates/{id}/edit' => ['controller' => 'UpdateController', 'action' => 'editPage','guard' => 'Authenticated'],
+    '/updates/{id}/doedit' => ['controller' => 'UpdateController', 'action' => 'editAction','guard' => 'Authenticated'],
+    '/updates/{id}/delete' => ['controller' => 'UpdateController', 'action' => 'deleteUpdate','guard' => 'Authenticated'],
+    '/updates/add' => ['controller' => 'UpdateController', 'action' => 'addUpdate','guard' => 'Authenticated'],
     '/' =>['controller' => 'PageController', 'action' => 'home']
 ];
